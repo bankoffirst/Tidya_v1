1abed4e06b89bb05d36137fba547c8680267d46d
Phoom <Phoombnw@gmail.com>
2023-03-31 20:27:26 +0700

    -update preview

--- app/src/main/java/com/example/tidya/presentation/HistoryScreen.kt
+++ app/src/main/java/com/example/tidya/presentation/HistoryScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.unit.sp
 import com.example.tidya.model.User
 
 @Composable
-fun HomeScreen(user: User){
+fun HistoryScreen(user: User){
     Scaffold {innerPadding ->
         Column(modifier = Modifier
             .fillMaxSize()
